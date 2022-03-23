22df8b2925983c8dd224b81b700de94730730cf1
Seungwoo Sung <tjdtmddn1234@naver.com>
2022-03-23 12:33:33 +0900

    HW2

--- HW2_SEUNGWOO_SUNG/first_client.py
+++ HW2_SEUNGWOO_SUNG/first_client.py
@@ -1,7 +1,17 @@
 import socket
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('localhost', 9000)
 sock.connect(addr)
 msg = sock.recv(1024)
 print(msg.decode())
+
+name = "Seungwoo Sung"
+sock.send(name.encode('utf-8'))
+
+student_number = sock.recv(1024)
+
+student_number2 = int.from_bytes(student_number, 'big')
+print(student_number2)
+
 sock.close()
--- HW2_SEUNGWOO_SUNG/first_server.py
+++ HW2_SEUNGWOO_SUNG/first_server.py
@@ -7,4 +7,12 @@ while True:
     client, addr = s.accept()
     print('Connection from', addr)
     client.send(b'Hello ' + addr[0].encode())
+
+    name = client.recv(1024)
+    print(name.decode('utf-8'))
+
+    student_number = 20171516
+    student_number2 = student_number.to_bytes(4, 'big')
+    client.send(student_number2)
+
     client.close()
