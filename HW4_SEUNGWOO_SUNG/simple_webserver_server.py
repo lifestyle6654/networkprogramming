5b8b7b997dd2fd060746a46dca02e5aa1610b673
Seungwoo Sung <tjdtmddn1234@naver.com>
2022-03-29 18:31:22 +0900

    HW4

---
+++ HW4_SEUNGWOO_SUNG/simple_webserver_server.py
@@ -0,0 +1,49 @@
+from socket import *
+
+s = socket()
+s.bind(('', 80))
+s.listen(10)
+
+while True:
+    c, addr = s.accept()
+
+    data = c.recv(1024)
+    msg = data.decode()
+    req = msg.split('\r\n')
+    req2 = req[0].split(' ')
+    req3 = req2[1]
+    req4 = req3[1:] # index.html parsing
+
+    if req4 == 'index.html':
+        f = open(req4, 'r', encoding='utf-8')
+        mimeType = 'text/html'
+        c.send('HTTP/1.1 200 OK\r\n'.encode())
+        c.send('Content-Type: '.encode() + mimeType.encode() + '\r\n'.encode())
+        c.send('\r\n'.encode())
+        data = f.read()
+        c.send(data.encode('euc-kr'))
+    
+    elif req4 == 'iot.png':
+        f = open(req4, 'rb')
+        mimeType = 'image/png'
+        c.send('HTTP/1.1 200 OK\r\n'.encode())
+        c.send('Content-Type: '.encode() + mimeType.encode() + '\r\n'.encode())
+        c.send('\r\n'.encode())
+        data = f.read()
+        c.send(data)
+
+    elif req4 == 'favicon.ico':
+        f = open(req4, 'rb')
+        mimeType = 'image/x-icon'
+        c.send('HTTP/1.1 200 OK\r\n'.encode())
+        c.send('Content-Type: '.encode() + mimeType.encode() + '\r\n'.encode())
+        c.send('\r\n'.encode())
+        data = f.read()
+        c.send(data)
+    else :
+        c.send('HTTP/1.1 404 Not Found\r\n'.encode())
+        c.send('\r\n'.encode())
+        c.send('<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>'.encode())
+        c.send('<BODY>Not Found</BODY></HTML>'.encode())
+
+    c.close()
